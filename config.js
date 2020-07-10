cfe2cc10474b07c4a225335488b4b716421242d4
Liam <terroridk@gmail.com>
2020-07-09 21:34:55 -0400

    minor UI update

--- config.js
+++ config.js
@@ -1,52 +1,52 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 
-'use strict';
+"use strict";
 
 module.exports = {
-	url: 'https://lumen.netlify.com',
-	pathPrefix: '/',
-	title: 'Liam Scalzulli',
-	subtitle: 'Personal Site',
-	copyright: '© All rights reserved.',
-	disqusShortname: '',
-	postsPerPage: 4,
-	googleAnalyticsId: 'UA-151954055-2',
-	useKatex: false,
-	menu: [
-		{
-			label: 'Articles',
-			path: '/'
-		},
-		{
-			label: 'About me',
-			path: '/pages/about'
-		},
-		{
-			label: 'Projects',
-			path: '/pages/projects'
-		}
-	],
-	author: {
-		name: 'Liam Scalzulli',
-		photo: '/avi.gif',
-		bio: 'Aspiring Software Engineer, Competitive Programmer, Student',
-		contacts: {
-			email: 'liam.scalzulli@johnabbottcollege.net',
-			facebook: '',
-			telegram: '',
-			twitter: '',
-			github: 'terror',
-			rss: '',
-			vkontakte: '',
-			linkedin: 'liam-scalzulli',
-			instagram: '',
-			line: '',
-			gitlab: '',
-			weibo: '',
-			codepen: '',
-			youtube: '',
-			soundcloud: ''
-		}
-	}
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Liam Scalzulli",
+  subtitle: "Personal Site",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-151954055-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Posts",
+      path: "/",
+    },
+    {
+      label: "About",
+      path: "/pages/about",
+    },
+    {
+      label: "Projects",
+      path: "/pages/projects",
+    },
+  ],
+  author: {
+    name: "Liam Scalzulli",
+    photo: "/space.jpeg",
+    bio: "Aspiring Software Engineer, Competitive Programmer, Student",
+    contacts: {
+      email: "liam.scalzulli@johnabbottcollege.net",
+      facebook: "",
+      telegram: "",
+      twitter: "",
+      github: "terror",
+      rss: "",
+      vkontakte: "",
+      linkedin: "liam-scalzulli",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
